1bb950df7ddae48c59ee242c7916fdd6a1542bf6
Khalidur34 <khalidur34@gmail.com>
2025-02-01 23:02:25 +1100

    functions createForm() and createFormDiv() implemented

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const myLibrary = [];
+const addButton = document.querySelector('.addButton');
+const books = document.querySelector('#books');
 
 function Book(name, author, pageCount) {
     this.name = name;
@@ -6,10 +8,45 @@ function Book(name, author, pageCount) {
     this.pageCount = pageCount;
 }
 
+function createForm() {
+    const form = document.createElement('form');
+    const name = createFormDiv("bookName", "Name", "text");
+    const author = createFormDiv("authorName", "Author", "text");
+    const pageCount = createFormDiv("pageCount", "Pages", "number");
+
+    form.appendChild(name);
+    form.appendChild(author);
+    form.appendChild(pageCount);
+
+    return form;
+}
+
 function addBookToLibrary(name, author, pageCount) {
     let book = new Book(name, author, pageCount);
     myLibrary.push(book);
 }
 
-addBookToLibrary("book", "khalid", 100);
-console.log(myLibrary);
+addButton.addEventListener('click', function() {
+    console.log("Button was Clicked");
+});
+
+function createFormDiv(id, labelName, inputType) {
+    const formDiv = document.createElement('div');
+    
+    const label = document.createElement('label');
+    label.setAttribute("for", id);
+    label.textContent = labelName;
+    
+    const input = document.createElement('input');
+    input.setAttribute("type", inputType);
+    input.setAttribute("name", id);
+    input.setAttribute("id", id);
+
+    formDiv.appendChild(label);
+    formDiv.appendChild(input);
+
+    return formDiv;
+}
+
+console.log(createForm());
+
