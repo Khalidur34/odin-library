ab82cd8d22bd214e72ae14a1562c26f684737233
Khalidur34 <khalidur34@gmail.com>
2025-02-02 23:51:15 +1100

    add button and new book form implemented

--- script.js
+++ script.js
@@ -8,29 +8,28 @@ function Book(name, author, pageCount) {
     this.pageCount = pageCount;
 }
 
-function createForm() {
-    const form = document.createElement('form');
-    const name = createFormDiv("bookName", "Name", "text");
-    const author = createFormDiv("authorName", "Author", "text");
-    const pageCount = createFormDiv("pageCount", "Pages", "number");
-
-    form.appendChild(name);
-    form.appendChild(author);
-    form.appendChild(pageCount);
-
-    return form;
-}
-
 function addBookToLibrary(name, author, pageCount) {
     let book = new Book(name, author, pageCount);
     myLibrary.push(book);
 }
 
 addButton.addEventListener('click', function() {
-    console.log("Button was Clicked");
+    const formDiv = createFormDiv();
+    books.appendChild(formDiv);
+    formDiv.addEventListener('submit', function(e) {
+        e.preventDefault();
+        //console.log(document.querySelector('[name = "bookName"]').value);
+        bookName = document.querySelector('[name = "bookName"]').value ;
+        authorName = document.querySelector('[name = "authorName"]').value;
+        pageCount = document.querySelector('[name = "pageCount"]').value;
+
+        let book = createBookDiv(bookName, authorName, pageCount);
+        books.removeChild(formDiv);
+        books.insertBefore(book, books.firstChild);
+    });    
 });
 
-function createFormDiv(id, labelName, inputType) {
+function createFormFieldDiv(id, labelName, inputType) {
     const formDiv = document.createElement('div');
     
     const label = document.createElement('label');
@@ -48,5 +47,44 @@ function createFormDiv(id, labelName, inputType) {
     return formDiv;
 }
 
-console.log(createForm());
+function createFormDiv() {
+    const formDiv = document.createElement('div');
+    const form = document.createElement('form');
+    const name = createFormFieldDiv("bookName", "Book Name", "text");
+    const author = createFormFieldDiv("authorName", "Author", "text");
+    const pageCount = createFormFieldDiv("pageCount", "Pages", "number");
+
+    form.appendChild(name);
+    form.appendChild(author);
+    form.appendChild(pageCount);
+
+    const buttonDiv = document.createElement('div');
+    const button = document.createElement('button');
+    button.setAttribute('type', 'submit');
+    button.textContent = 'Create';
+    buttonDiv.appendChild(button);
+    form.appendChild(buttonDiv);
+
+    formDiv.classList.add('card', 'form');
+    formDiv.appendChild(form);
+
+    return formDiv;
+}
+
+function createBookDiv(bookName, authorName, pageCount) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    const cardName = document.createElement('div');
+    cardName.textContent = bookName;
+    const cardAuthor = document.createElement('div');
+    cardAuthor.textContent = authorName;
+    const cardPageCount = document.createElement('div');
+    cardPageCount.textContent = pageCount;
+
+    card.appendChild(cardName);
+    card.appendChild(cardAuthor);
+    card.appendChild(cardPageCount);
+
+    return card;
+}
 
